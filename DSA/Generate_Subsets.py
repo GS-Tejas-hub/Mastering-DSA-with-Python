076cfd27f6536cc866150963efa073a288d51c9b
G S Tejas <127507929+GS-Tejas-hub@users.noreply.github.com>
2024-07-23 11:18:25 +0530

    Delete DSA directory

--- DSA/Generate_Subsets.py
+++
@@ -1,8 +0,0 @@
-def generate_subsets(arr, index=0, current=[]):
-    if index == len(arr):
-        print(current)
-        return
-    generate_subsets(arr, index + 1, current)
-    generate_subsets(arr, index + 1, current + [arr[index]])
-arr = [1, 2, 3, 4]
-generate_subsets(arr)
--- DSA/Sum_Of_arrays.py
+++
@@ -1,7 +0,0 @@
-def Sum_Of_array(arr, index=0):
-    if index == len(arr):
-        return 0
-    else:
-        return arr[index] + Sum_Of_array(arr, index+1)
-arr = [8,7,1,1,9,8]
-print(Sum_Of_array(arr))
